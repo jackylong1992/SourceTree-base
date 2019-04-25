af5342ae594e764696083d4d756c76bab9512db2
jackylong1992 <jklongnguyen@gmail.com>
2019-04-25 08:58:44 +0700

    good start

--- script/main.js
+++ script/main.js
@@ -1,1 +1,37 @@
-console.log("run to main.js");
+var g_foodEnergy = [];
+$.get("./food.json", (data)=> {
+    g_foodEnergy = data;
+    showTable($(".table.list"), data);
+    $(".table.list").hide();
+})
+
+$(".show-list").on("click", ()=> {
+    $(".table.list").toggle();
+})
+
+$("#search").on("input", function() {
+    var searchKey = $(this).val();
+    var searchResultArr = [];
+    if (searchKey && searchKey.length) {
+        searchResultArr = g_foodEnergy.filter(function(value){
+            return value.name.includes(searchKey);
+        });
+    }
+    
+    showTable($(".table.search"), searchResultArr);
+})
+
+
+
+function showTable (jQueryTableElement, data) {
+    var bodyText = "<table><thead><tr><th>Tên thức ăn</th><th>Calo</th><th>Unit</th></tr></thead><tbody>";
+    data.forEach((foodItem)=>{
+        bodyText += "<tr>" + generateName(foodItem) + "</tr>"
+    });
+    bodyText += "</tbody></table>";
+    jQueryTableElement.html(bodyText);
+}
+
+function generateName (item) {
+    return "<td>" + item.name + "</td>" + "<td>" + item.calo + "</td>" + "<td>" + item.unit + "</td>";
+}

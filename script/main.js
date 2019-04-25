4c7890182a924d545c5c3bf8c78dddbf07bfdf8d
jackylong1992 <jklongnguyen@gmail.com>
2019-04-25 14:54:21 +0700

    very good

--- script/main.js
+++ script/main.js
@@ -105,7 +105,7 @@ function chooseFood (event) {
     }
     // console.log(newItem);
     g_todayFood.push(newItem);
-    showTodayTable($(".table.food"), g_todayFood)
+    showTodayTable($(".table.food"), g_todayFood);
 }
 
 function generateName (item) {
@@ -137,3 +137,70 @@ function generateSummary (item) {
             <td>   </td>
             <td>   </td>`;
 }
+
+function exportToJsonFile(jsonData) {
+    let dataStr = JSON.stringify(jsonData);
+    let dataUri = 'data:application/json;charset=utf-8,'+ encodeURIComponent(dataStr);
+    let fileName = $("#ouputName").val()
+    let exportFileDefaultName = fileName ? fileName  : 'data';
+    exportFileDefaultName += ".json";
+    
+    let linkElement = document.createElement('a');
+    linkElement.setAttribute('href', dataUri);
+    linkElement.setAttribute('download', exportFileDefaultName);
+    linkElement.click();
+}
+
+$(".save").on("click", function() {
+    exportToJsonFile(g_todayFood);
+})
+
+function initializeTodayFood(jsonValue) {
+    g_todayFood = JSON.parse(jsonValue);
+    showTodayTable($(".table.food"), g_todayFood);
+}
+
+//////////////
+
+
+function handleFileSelect(evt) {
+	var files = evt.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+	var files = evt.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+	var output = [];
+	for (var i = 0, f; f = files[i]; i++) {
+		var reader = new FileReader();
+
+		// Closure to capture the file information.
+		reader.onload = (function (theFile) {
+			return function (e) {
+                console.log(e.target.result);
+                initializeTodayFood(e.target.result);
+
+				// console.log('e readAsText = ', e);
+                // console.log('e readAsText target = ', e.target);
+
+                
+				// try {
+				// 	json = JSON.parse(e.target.result);
+				// 	alert('json global var has been set to parsed json of this file here it is unevaled = \n' + JSON.stringify(json));
+				// } catch (ex) {
+				// 	alert('ex when trying to parse json = ' + ex);
+				// }
+			}
+		})(f);
+		reader.readAsText(f);
+	}
+
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
